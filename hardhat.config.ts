84471afeaa3ade38b218f64cf97daf85d50da83b
heyitaki <akshath@berkeley.edu>
2021-11-28 14:46:56 -0800

    verify contract on etherscan

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,4 @@
+import '@nomiclabs/hardhat-etherscan';
 import '@nomiclabs/hardhat-waffle';
 import '@typechain/hardhat';
 import dotenv from 'dotenv';
@@ -6,6 +7,9 @@ import { HardhatUserConfig } from 'hardhat/config';
 dotenv.config();
 
 const config: HardhatUserConfig = {
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
   networks: {
     mainnet: {
       url: process.env.ALCHEMY_HTTP_ENDPOINT,
@@ -25,7 +29,6 @@ const config: HardhatUserConfig = {
     outDir: 'typechain',
     target: 'ethers-v5',
     alwaysGenerateOverloads: false,
-    externalArtifacts: ['externalArtifacts/*.json'],
   },
 };
 
