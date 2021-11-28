c800f4e623050304f3a4eefeb26e9e7905f85f70
heyitaki <akshath@berkeley.edu>
2021-11-27 21:45:39 -0800

    deploy script + config stuff to prep deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,25 @@
 import '@nomiclabs/hardhat-waffle';
+import dotenv from 'dotenv';
+import { HardhatUserConfig } from 'hardhat/config';
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
-  solidity: '0.8.0',
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  networks: {
+    mainnet: {
+      url: process.env.ALCHEMY_HTTP_ENDPOINT,
+      accounts: [process.env.PRIVATE_KEY || ''],
+    },
+  },
+  solidity: {
+    version: '0.8.0',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 3,
+      },
+    },
+  },
 };
+
+export default config;
